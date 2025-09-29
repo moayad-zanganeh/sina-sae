b08b10954a2285b8b035d655b1e96f98279b7494
moayad-zanganeh <moayadzangane@gmail.com>
2025-09-29 17:07:33 +0330

    feat: edit commponent home

--- src/components/home/main-bar/music/index.tsx
+++ src/components/home/main-bar/music/index.tsx
@@ -111,7 +111,7 @@ function Music() {
                   transition: 'transform 0.5s ease',
                   zIndex,
                   borderRadius: '15px',
-                  height: { lg: '70%', md: '59%', sm: '44%', xs: '24%' },
+                  height: { lg: '67%', md: '53%', sm: '35%', xs: '21%' },
                   width: '40%',
                 }}
               >
